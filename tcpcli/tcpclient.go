d0726761089ac0632ffb6731885736eff55d00c7
egitop <egitop@uroad.xyz>
2026-05-09 00:35:43 +0800

    add tcp msg demo

--- tcpcli/tcpclient.go
+++ tcpcli/tcpclient.go
@@ -5,12 +5,15 @@ import (
 	"encoding/hex"
 	"log"
 	"strings"
+	"net"
+	"time"
 )
 
 /*
    #cgo LDFLAGS: -ltoxcore
 
    #include <stdlib.h>
+   #include <dlfcn.h>
    #include "tcpclient.h"
 */
 import "C"
@@ -29,12 +32,91 @@ type ConnPool struct {
 	Proxy  voidptr
 
 	Conns voidptr
+
+	relays  []*node
+	peerConns1 map[int]*peer_connection
+	peerConns2 map[string]*peer_connection
 }
 
 func New(keyfile string) *ConnPool {
 	p := new_pool(keyfile)
+	p.peerConns1 = make(map[int]*peer_connection)
+	p.peerConns2 = make(map[string]*peer_connection)
+	p.setup_callbacks()
 	return p
 }
+
+type node struct {
+	ip string
+	port int
+	pk string
+}
+
+func (p *ConnPool) AddTcpRelay(ip string, port int, pk string) error {
+	ipport := C.Tox_IP_Port{}
+	C.add_tcp_relay_global(p.Conns, voidptr(&ipport),nil)
+	return nil
+}
+
+type peer_connection struct {
+	peerpk string
+	connid int
+	readch chan any
+	p *ConnPool
+}
+
+func (c *peer_connection) Close() error {
+	return nil
+}
+
+func (c *peer_connection) Write(buf []byte) error {
+	return nil
+}
+
+func (c *peer_connection) Read(buf []byte) error {
+	return nil
+}
+
+func (p *ConnPool) Dial(peerpk string) (net.Conn,error) {
+	return nil,nil
+}
+
+func (p *ConnPool) send() error {
+	return nil
+}
+
+/////
+
+var _ net.PacketConn = (*ConnPool)(nil)
+func (p *ConnPool) WriteTo(buf []byte, addr net.Addr) (int, error) {
+	return 0, nil
+}
+
+func (p *ConnPool) ReadFrom(buf []byte) (int, net.Addr, error){
+	return 0, nil, nil
+}
+func (p *ConnPool) Close() error {
+	return nil
+}
+func (p *ConnPool) LocalAddr() net.Addr {
+	return nil
+}
+func (p *ConnPool) SetDeadline(v time.Time) error {
+	return nil
+}
+func (p *ConnPool) SetReadDeadline(v time.Time) error {
+	return nil
+}
+func (p *ConnPool) SetWriteDeadline(v time.Time) error {
+	return nil
+}
+
+func (p *ConnPool) send_oob(peerpk string, data string) error {
+	return nil
+}
+
+
+/////
 
 func new_pool(keyfile string) *ConnPool {
 
@@ -47,10 +129,10 @@ func new_pool(keyfile string) *ConnPool {
 	pxy := C.malloc(256)
 	defer C.free(pxy)
 
-	_, sk := keyfile_neworload(keyfile, rng)
+	pk, sk := keyfile_neworload(keyfile, rng)
 	skb, err := hex.DecodeString(sk)
 	if err != nil { log.Fatalln(err) }
-	log.Println(sk)
+	log.Printf("pk=%v\n", pk)
 
 	o := C.new_tcp_connections(logger, mem, rng, ns,
 		mono_time, voidptr(&skb[0]), pxy, np)
@@ -85,4 +167,30 @@ func keyfile_neworload(keyfile string, rng voidptr) (string, string) {
 	r1 := hex.EncodeToString(sk[:])
 
 	return strings.ToUpper(r0), strings.ToUpper(r1)
+}
+
+func (p *ConnPool) setup_callbacks() {
+	f1 := dlsym0("toxin_on_oob_packet_bygo")
+	C.set_oob_packet_tcp_connection_callback(p.Conns, f1, p.Conns)
+	f2 := dlsym0("toxin_on_data_packet_bygo")
+	C.set_oob_packet_tcp_connection_callback(p.Conns, f2, p.Conns)
+}
+
+//export toxin_on_oob_packet_bygo
+func toxin_on_oob_packet_bygo(obj voidptr, pubkey voidptr, tcp_connections_number int, packet voidptr, length uint16, userdata voidptr) {
+	log.Printf("%v %v %v\n", tcp_connections_number, packet, length)
+}
+
+
+func dlsym0(name string) voidptr {
+	namec := C.CString(name)
+	defer C.free(voidptr(namec))
+
+	f1 := C.dlsym(C.RTLD_DEFAULT, namec)
+	return f1
+}
+
+//export toxin_on_data_packet_bygo
+func toxin_on_data_packet_bygo(object voidptr, crypt_connection_id int, packet voidptr, length uint16, userdata voidptr) {
+	log.Printf("%v %v %v\n", crypt_connection_id, packet, length)
 }
